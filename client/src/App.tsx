bce462d1d1fef5dc25f6700b48793f36af82b3fd
Velislav21 <zirovcho34@gmail.com>
2025-04-15 12:05:04 +0300

    adding footer

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import SiteHeader from "./components/site-header/SiteHeader";
 import FeaturedRecipes from "./components/featured-recipes/FeaturedRecipes";
 import Categories from "./components/categories/Categories";
 import PopularRecipes from "./components/popular-recipes/PopularRecipes";
+import Footer from "./components/footer/Footer";
 
 function App() {
     return (
@@ -12,6 +13,7 @@ function App() {
                 <Categories />
                 <PopularRecipes />
             </main>
+            <Footer />
         </>
     );
 }
---
+++ client/src/components/footer/Footer.tsx
@@ -0,0 +1,9 @@
+import styles from "./Footer.module.css";
+
+export default function Footer() {
+    return (
+        <footer className={styles["site-footer"]}>
+            <p>&#169; All rights reserved.</p>
+        </footer>
+    )
+}
