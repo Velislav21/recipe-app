27982741a0f6610241b530701d35ef303cdc7a15
Velislav21 <zirovcho34@gmail.com>
2025-04-17 12:02:28 +0300

    initializing the backend for the app

---
+++ server/dist/index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+const app = express();
+app.use(cors({
+    origin: '*',
+    credentials: true,
+}));
+app.use(express.json());
+app.get("/", (req, res) => {
+    res.send({ message: "works" });
+});
+app.listen(8080, () => console.log(`Server is listening on http://localhost:${8080}`)); // !TODO .env ??
+//# sourceMappingURL=index.js.map
