28be0ac81ec997d9ec55f45a6ded233a104d0a0c
Velislav21 <zirovcho34@gmail.com>
2025-04-17 12:59:43 +0300

    installing and testing axios

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,31 @@
-import express from 'express';
-import cors from 'cors'
-import 'dotenv/config'
+import express from "express";
+import cors from "cors";
+import axios from "axios";
+import "dotenv/config";
 const app = express();
 
-app.use(cors({
-    origin: '*',  
-    credentials: true,
-}))
+app.use(
+    cors({
+        origin: "*",
+        credentials: true,
+    })
+);  
 
-app.use(express.json())
+app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send({message: "works"})
-})
+    axios
+        .get("https://ss-server-731733900032.europe-west4.run.app/shoes")
+        .then((result) => {
+            console.log(result.data);
+        })
+        .finally(() => console.log("request finished"))
+    res.send({ message: "works" });
+});
 
-app.listen(8080, () => console.log(`Server is listening on http://localhost:${8080}`)) // !TODO .env ??
+app.get("/recipe/:recipeId", (req, res) => {});
+app.get("/category/:category", (req, res) => {});
+
+app.listen(8080, () =>
+    console.log(`Server is listening on http://localhost:${8080}`)
+); // !TODO .env ??
