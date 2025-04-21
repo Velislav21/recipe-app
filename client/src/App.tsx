50355bb7a476f77ed89036be63b1fcd3aff34f9f
Velislav21 <zirovcho34@gmail.com>
2025-04-21 10:55:19 +0300

    setting up tanstack query

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,16 +5,22 @@ import CategoryPage from "./components/categories/category-specific-page/Categor
 
 import { Routes, Route } from "react-router";
 
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools"
+
+const queryClient = new QueryClient();
+
 function App() {
     return (
-        <>
+        <QueryClientProvider client={queryClient} >
             <Routes>
                 <Route path="/" Component={Home} />
                 <Route path="/recipe/:recipeId" Component={RecipeDetails} />
                 <Route path="/category/:category" Component={CategoryPage} />
             </Routes>
             <Footer />
-        </>
+            <ReactQueryDevtools buttonPosition="bottom-left"/>
+        </QueryClientProvider>
     );
 }
 
