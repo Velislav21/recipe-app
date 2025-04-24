e0e1003055d8b6fa110ac0ca463d9a5b548193e8
Velislav21 <zirovcho34@gmail.com>
2025-04-24 18:37:38 +0300

    added a load more btn component

---
+++ client/src/components/ui/load-more-btn/LoadMoreBtn.tsx
@@ -0,0 +1,26 @@
+import { FetchNextPageOptions } from "@tanstack/react-query";
+import styles from "./LoadMoreBtn.module.css"
+
+type LoadMoreBtnProps = {
+    fetchNextPage: (options?: FetchNextPageOptions) => {},
+    hasNextPage: boolean,
+    isFetchingNextPage: boolean,
+}
+
+export default function LoadMoreBtn({fetchNextPage, hasNextPage, isFetchingNextPage}: LoadMoreBtnProps) {
+    return (
+        <div className={styles["pagination-container"]}>
+            <button
+                onClick={() => fetchNextPage()}
+                disabled={!hasNextPage || isFetchingNextPage}
+                className={styles["load-more-button"]}
+            >
+                {isFetchingNextPage
+                    ? "Loading more..."
+                    : hasNextPage
+                    ? "Load More Recipes"
+                    : "No more recipes to load"}
+            </button>
+        </div>
+    );
+}
