778ade89987b8294ab046c6576c66ad664d58100
Velislav21 <zirovcho34@gmail.com>
2025-04-26 17:57:09 +0300

    adding some error handling on the backend

--- server/src/controllers/recipeController.ts
+++ server/src/controllers/recipeController.ts
@@ -1,19 +1,22 @@
 import { Request, Response, Router } from "express";
 
+import { isAxiosError } from "axios";
 import axiosClient from "../axiosInstance.js";
 
-import { Recipes, RecipeDetails, CategoryBasedRecipes } from "../types/recipes.js";
-
+import {
+    Recipes,
+    RecipeDetails,
+    CategoryBasedRecipes,
+} from "../types/recipes.js";
 
 const recipeController = Router();
 
 recipeController.get("/", async (req: Request, res: Response) => {
-
-    const searchTerm = req.query.searchTerm as string;
+    const searchTerm = req.query.searchTerm;
     let url = "/recipes/random/?number=7";
 
     if (searchTerm) {
-        url += `&include-tags=${searchTerm}`
+        url += `&include-tags=${searchTerm}`;
     }
 
     try {
@@ -23,50 +26,111 @@ recipeController.get("/", async (req: Request, res: Response) => {
             popularRecipes: response.data.recipes,
         });
     } catch (error) {
-        console.log(error) //!TODO fix this
-    }
-});
-
-recipeController.get("/details/:recipeId", async (req: Request, res: Response) => {
-    const recipeId = req.params.recipeId
- 
-    try {
-        const response = await axiosClient.get<RecipeDetails>(`/recipes/${recipeId}/information`);
-
-        if (!response.data || typeof response.data !== "object") {
-            res.status(502).json({ message: "Invalid API response"})
+        if (isAxiosError(error)) {
+            if (error.response) {
+                if (error.response.status === 402) {
+                    res.status(402).json({
+                        message: "API token limit reached.",
+                    });
+                    return;
+                }
+                res.status(error.response.status).json(
+                    error.response.data || {
+                        message: "Error fetching from API",
+                    }
+                );
+                return;
+            }
+        } else {
+            res.status(500).json({ message: "Internal server error." });
+            return;
         }
+    }
+});
 
-        const filteredData: RecipeDetails = {
-            id: response.data.id,
-            image: response.data.image,
-            title: response.data.title,
-            readyInMinutes: response.data.readyInMinutes,
-            servings: response.data.servings,
-            analyzedInstructions: response.data.analyzedInstructions,
-            extendedIngredients: response.data.extendedIngredients,
+recipeController.get(
+    "/details/:recipeId",
+    async (req: Request, res: Response) => {
+        const recipeId = req.params.recipeId;
+
+        try {
+            const response = await axiosClient.get<RecipeDetails>(
+                `/recipes/${recipeId}/information`
+            );
+
+            if (!response.data || typeof response.data !== "object") {
+                res.status(502).json({ message: "Invalid API response" });
+            }
+
+            const filteredData: RecipeDetails = {
+                id: response.data.id,
+                image: response.data.image,
+                title: response.data.title,
+                readyInMinutes: response.data.readyInMinutes,
+                servings: response.data.servings,
+                analyzedInstructions: response.data.analyzedInstructions,
+                extendedIngredients: response.data.extendedIngredients,
+            };
+            res.status(200).json(filteredData);
+        } catch (error) {
+            if (isAxiosError(error)) {
+                if (error.response) {
+                    if (error.response.status === 402) {
+                        res.status(402).json({
+                            message: "API token limit reached.",
+                        });
+                        return;
+                    }
+                    res.status(error.response.status).json(
+                        error.response.data || {
+                            message: "Error fetching from API",
+                        }
+                    );
+                    return;
+                }
+            } else {
+                res.status(500).json({ message: "Internal server error." });
+                return;
+            }
         }
-        res.status(200).json(filteredData);
-    } catch (error) {
-        console.log(error) //!TODO fix this
     }
-});
+);
 
-recipeController.get("/category/:category", async (req: Request, res: Response) => {
+recipeController.get(
+    "/category/:category",
+    async (req: Request, res: Response) => {
+        const category = req.params.category;
 
-    const category = req.params.category;
+        const offset = parseInt((req.query.offset as string) || "0", 10);
+        const number = parseInt((req.query.number as string) || "12", 10);
 
-    const offset = parseInt(req.query.offset as string || '0', 10);
-    const number = parseInt(req.query.number as string || '12', 10);
-
-    try {
-        const response =
-            await axiosClient.get<CategoryBasedRecipes>
-            (`/recipes/complexSearch?type=${category}&addRecipeInformation=true&offset=${offset}&number=${number}`);
-        res.status(200).json(response.data);
-    } catch (error) {
-        console.log(error) //!TODO fix this
+        try {
+            const response = await axiosClient.get<CategoryBasedRecipes>(
+                `/recipes/complexSearch?type=${category}&addRecipeInformation=true&offset=${offset}&number=${number}`
+            );
+            res.status(200).json(response.data);
+        } catch (error) {
+            if (isAxiosError(error)) {
+                if (error.response) {
+                    if (error.response.status === 402) {
+                        res.status(402).json({
+                            message: "API token limit reached.",
+                        });
+                        return;
+                    }
+                    res.status(error.response.status).json(
+                        error.response.data || {
+                            message: "Error fetching from API",
+                        }
+                    );
+                    return;
+                }
+            } else {
+                res.status(500).json({ message: "Internal server error." });
+                return;
+            }
+        }
     }
-});
+);
 
 export default recipeController;
